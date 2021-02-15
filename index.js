4e7529c3c985f16bf0634df43596adfc89ff4dad
kriseliassen <kristin.eliassen@gmail.com>
2021-02-15 19:39:11 +0100

    Added movement and restrictions to ghosts - javascript

--- index.js
+++ index.js
@@ -151,4 +151,39 @@ const ghosts = [
 ]
 
 //Draw ghosts onto grid
-ghosts.forEach(ghost => squares[ghost.startIndex].classList.add(ghost.className))
+ghosts.forEach(ghost => {
+    squares[ghost.currentIndex].classList.add(ghost.className)
+    squares[ghost.currentIndex].classList.add("ghost")
+})
+
+//move the ghosts
+ghosts.forEach(ghost => moveGhost(ghost))
+
+
+function moveGhost(ghost) {
+    console.log("moved ghost")
+    const directions = [-1, +1, -width, +width]
+    let direction = directions[Math.floor(Math.random() * directions.length)]
+    console.log(direction)
+
+    ghost.timerID = setInterval(function() {
+        if(
+            !squares[ghost.currentIndex + direction].classList.contains("ghost") &&
+            !squares[ghost.currentIndex + direction].classList.contains("wall")
+        ) {
+              squares[ghost.currentIndex].classList.remove(ghost.className)
+              squares[ghost.currentIndex].classList.remove("ghost")
+               ghost.currentIndex += direction
+               squares[ghost.currentIndex].classList.add(ghost.className)
+               squares[ghost.currentIndex].classList.add("ghost")
+        } else direction = directions[Math.floor(Math.random() * directions.length)]
+    }, ghost.speed)
+}
+
+
+
+
+
+
+
+//A+shift+option comment out block code
