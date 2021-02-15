f31927b8d9702939239c48740acfec0bfe7a6272
kriseliassen <kristin.eliassen@gmail.com>
2021-02-15 18:23:07 +0100

    Kept Pacman inside walls - javascript

--- index.js
+++ index.js
@@ -74,22 +74,30 @@ function control(e) {
     switch(e.keyCode) {
         case 40:
             console.log('pressed down')
-            if(pacmanCurrentIndex + width < width*width) pacmanCurrentIndex += width
+            if(
+                !squares[pacmanCurrentIndex + width].classList.contains("wall") && 
+                pacmanCurrentIndex + width < width*width) pacmanCurrentIndex += width
         break
 
         case 39:
             console.log('pressed right')
-            if (pacmanCurrentIndex % width < width-1) pacmanCurrentIndex +=1
+            if (
+                !squares[pacmanCurrentIndex +1].classList.contains("wall") &&
+                pacmanCurrentIndex % width < width-1) pacmanCurrentIndex +=1
         break
         
         case 38:
             console.log('pressed up')
-            if(pacmanCurrentIndex - width >=0) pacmanCurrentIndex -=width
+            if(
+                !squares[pacmanCurrentIndex - width].classList.contains("wall") &&
+                pacmanCurrentIndex - width >=0) pacmanCurrentIndex -=width
         break
 
         case 37:
             console.log('pressed left')
-            if(pacmanCurrentIndex % width !== 0) pacmanCurrentIndex -=1
+            if(
+                !squares[pacmanCurrentIndex - 1].classList.contains("wall") &&
+                pacmanCurrentIndex % width !== 0) pacmanCurrentIndex -=1
         break
     }
     squares[pacmanCurrentIndex].classList.add("pacman")
